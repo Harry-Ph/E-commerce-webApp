8db30d6eacf82ebc88776bdeac32e08145a3ad74
phphat <phphat.phanhongphat@gmail.com>
2020-07-17 02:14:09 +0300

    products page

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import useStyles from './style'
 import { Typography } from '@material-ui/core'
+import FacebookIcon from '@material-ui/icons/Facebook'
+import InstagramIcon from '@material-ui/icons/Instagram'
+import YouTubeIcon from '@material-ui/icons/YouTube'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
 
 export interface IFooterProps {}
 
@@ -11,29 +16,45 @@ const Footer = () => {
        <div className={classes.footer}>
             <div className={classes.footer__info}>
                 <div className={classes.info__store}>
-                    <Typography>GIFT CARDS</Typography>
-                    <Typography>FIND STORES</Typography>
-                    <Typography>SIGN UP FOR EMAIL</Typography>
-                    <Typography>BECOME A MEMBER</Typography>
-                    <Typography>STUDENT DISCOUNT</Typography>
+                    <Typography className={classes.footer__tittle}>GIFT CARDS</Typography>
+                    <Typography className={classes.footer__tittle}>FIND STORES</Typography>
+                    <Typography className={classes.footer__tittle}>SIGN UP FOR EMAIL</Typography>
+                    <Typography className={classes.footer__tittle}>BECOME A MEMBER</Typography>
+                    <Typography className={classes.footer__tittle}>STUDENT DISCOUNT</Typography>
                 </div>
                 <div className={classes.info}>
-                    <Typography>GET HELP</Typography>
-                    <Typography>Order Status</Typography>
-                    <Typography>Shipping and Delivery</Typography>
-                    <Typography>Returns</Typography>
-                    <Typography>Payment Options</Typography>
-                    <Typography>Contact Us</Typography>
+                    <Typography className={classes.footer__tittle}>GET HELP</Typography>
+                    <Typography className={classes.footer__modify}>Order Status</Typography>
+                    <Typography className={classes.footer__modify}>Shipping and Delivery</Typography>
+                    <Typography className={classes.footer__modify}>Returns</Typography>
+                    <Typography className={classes.footer__modify}>Payment Options</Typography>
+                    <Typography className={classes.footer__modify}>Contact Us</Typography>
                 </div>
                 <div className={classes.info}>
-                    <Typography>ABOUT US</Typography>
-                    <Typography>News</Typography>
-                    <Typography>Careers</Typography>
-                    <Typography>Investors</Typography>
-                    <Typography>Subtainablity</Typography>
+                    <Typography className={classes.footer__tittle}>ABOUT US</Typography>
+                    <Typography className={classes.footer__modify}>News</Typography>
+                    <Typography className={classes.footer__modify}>Careers</Typography>
+                    <Typography className={classes.footer__modify}>Investors</Typography>
+                    <Typography className={classes.footer__modify}>Subtainablity</Typography>
                 </div>
                 <div className={classes.footer__icon}>
+                    <FacebookIcon />
+                    <InstagramIcon />
+                    <YouTubeIcon />
+                    <TwitterIcon />
                 </div>
+            </div>
+            <div>
+                <hr/>
+            </div>
+            <div className={classes.footer__address}>
+                <Typography className={classes.footer__Location}><LocationOnIcon /></Typography>
+                <Typography className={classes.footer__Location}>Finland</Typography>
+                <Typography className={classes.footer__modify}>© 2020 Nike, Inc. All Rights Reserved</Typography>
+                <Typography className={classes.footer__modify}>Guides</Typography>
+                <Typography className={classes.footer__modify}>Terms of Use</Typography>
+                <Typography className={classes.footer__modify}>Terms of Sale</Typography>
+                <Typography className={classes.footer__modify}>Cookie Settings</Typography>
             </div>
        </div>
       )
