8afa70d73a122fd6a8df30c08fc40fad93ad0757
Thanh Dat PHAN <ptdat.thanhdatphan@gmail.com>
2020-08-04 19:59:44 +0300

    edit-product-page (#19)
    * Edit layout of product page.

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
-import {Modal, Backdrop, Fade, Button, CircularProgress} from '@material-ui/core';
+import {Modal, Backdrop, Fade, Button, CircularProgress, Box, FormGroup, TextField, MenuItem} from '@material-ui/core';
 
 import {request} from "graphql-request";
 import useSWR, {mutate, trigger} from 'swr'
 import useStyles from './style';
 import Router, {useRouter} from "next/router";
 import {Product} from "../../interfaces";
-import {FormikConfig, FormikValues} from "formik";
+import {ErrorMessage, Field, Form, Formik, FormikConfig, FormikValues} from "formik";
 import Loading from "../Loading";
+import {object, string} from "yup";
 
 const sleep = (time) => new Promise((acc) => setTimeout(acc, time));
 
@@ -40,9 +41,15 @@ export interface IModel {
   handleClose: (e: any)=> void
   redirectRouting:  (currentPage: string)=> void
   open: boolean
+  isEdit: boolean
 }
+
+const initialValues = {
+    name: '',
+}
+
 // @ts-ignore
-export default function TransitionsModal({redirectRouting, currentPage, handleOpen, product, handleClose, open}: IModel) {
+export default function TransitionsModal({redirectRouting, currentPage, handleOpen, product, handleClose, open, isEdit}: IModel) {
     const classes = useStyles();
     const [isLoading, setIsLoading] = useState(false);
     const handleRemoveInModal = async (e) => {
@@ -77,24 +84,60 @@ export default function TransitionsModal({redirectRouting, currentPage, handleOp
             >
                 <Fade in={open}>
                     <div className={classes.paper}>
-                        <h2 id="transition-modal-title">DELETE THIS PRODUCT</h2>
-                        <p id="transition-modal-description">Do you want to delete this item?.</p>
-                        <Button
-                          size="small"
-                          variant="contained"
-                          color="primary"
-                          onClick={handleRemoveInModal}
-                          startIcon={isLoading ? <CircularProgress size="1rem" /> : null}
-                          disabled={isLoading}
-                        >REMOVE</Button>
-                        <Button
-                          size="small"
-                          variant="outlined"
-                          color="default"
-                          type="submit"
-                          onClick={handleClose}
-                          disabled={isLoading}
-                        >CANCEL</Button>
+                        <h2 id="transition-modal-title">{isEdit ? 'EDIT' : 'DELETE'} THIS PRODUCT</h2>
+                        <p id="transition-modal-description">Do you want to {isEdit ? 'edit' : 'delete'} this item?.</p>
+                        {isEdit ?
+                            <Formik
+                                validationSchema={
+                                    object({
+                                        name: string().required().min(2).max(50),
+                                    })
+                                }
+                                initialValues={initialValues}
+                                onSubmit={() => {}}>
+                                {({isSubmitting}) => (
+                                    <Form>
+                                        <Box>
+                                            <FormGroup>
+                                                <Field name="name" as={TextField} label="Name"/>
+                                                <ErrorMessage name="name" />
+                                            </FormGroup>
+                                        </Box>
+                                        <Button size="small"
+                                                variant="contained"
+                                                color="primary"
+                                                type="submit" disabled={isSubmitting}>SUBMIT</Button>
+                                        <Button
+                                            size="small"
+                                            variant="outlined"
+                                            color="default"
+                                            type="submit"
+                                            onClick={handleClose}
+                                            disabled={isLoading}
+                                        >CANCEL</Button>
+                                    </Form>
+                                )}
+                            </Formik>
+                            :
+                            <>
+                                <Button
+                                    size="small"
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={handleRemoveInModal}
+                                    startIcon={isLoading ? <CircularProgress size="1rem" /> : null}
+                                    disabled={isLoading}
+                                >REMOVE</Button>
+                                <Button
+                                    size="small"
+                                    variant="outlined"
+                                    color="default"
+                                    type="submit"
+                                    onClick={handleClose}
+                                    disabled={isLoading}
+                                >CANCEL</Button>
+                            </>
+                        }
                     </div>
                 </Fade>
             </Modal>
